64d8250c65c5eeef0480b7564e2c6d797a45f7ce
Jake <jayhay28324@gmail.com>
2019-11-21 18:41:32 -0500

    Remove vue-bootstrap. Going with something else

--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import BootstrapVue from 'bootstrap-vue'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'devicons/css/devicons.css'
 import 'font-awesome/css/font-awesome.css'
 
-Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
